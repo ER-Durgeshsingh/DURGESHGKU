8e69041915df5c1dd3172daae84ac4024504c699
Durgesh Kumar <durgeshsingh23032002@gmail.com>
2026-05-15 15:17:42 +0530

    added gmail secrets and updated config

--- src/database/config.py
+++ src/database/config.py
@@ -2,8 +2,8 @@ import streamlit as st
 from supabase import create_client
 
 try:
-    SUPABASE_URL = st.secrets.get("SUPABASE_URL", "https://ultqgjwinmwowkcwxmpo.supabase.co")
-    SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", "sb_publishable_koh7aLEpmgwXKSL_MclJyA_oR2quZrA")
+    SUPABASE_URL = st.secrets["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
     supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
