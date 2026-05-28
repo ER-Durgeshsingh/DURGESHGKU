99af5734f63b3fbebf504b489b0b1b8ade7e3829
Durgeshwar Kumar Singh <durgeshsingh23032002@gmail.com>
2026-05-28 15:56:58 +0530

    Add Client import from supabase in config.py

--- src/database/config.py
+++ src/database/config.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from supabase import create_client
+from supabase import create_client, Client
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
